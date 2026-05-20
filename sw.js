73ec71fd2ad78fdb8ee4a0349412f3bd498bb6ff
1017yanagidasena-a11y <1017yanagidasena@gmail.com>
2026-05-20 19:05:10 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'noteview-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      return r || fetch(e.request);
+    })
+  );
+});
